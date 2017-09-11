1433aa439df9a501c0732d7b9ba2acbaf2f74222
hongchengguo <hongchengguo@szkuniu.com>
2017-09-11 20:20:56 +0800

    整理

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,58 +4,65 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    CharacterController cc;
-    public float speed = 20;
-    public float viewRange = 30;
-    public Chunk chunkPrefab;
+	#region 变量
+	private CharacterController cc;
+	public float speed = 20;
+	public float viewRange = 30;
+	public Chunk chunkPrefab;
+	#endregion
 
-    private void Start()
-    {
-        cc = GetComponent<CharacterController>();
-    }
+	#region INIT
+	private void Start()
+	{
+		cc = GetComponent<CharacterController>();
+	}
+	#endregion
 
-    void Update ()
-    {
-        UpdateInput();
-        UpdateWorld();
-    }
+	#region 逻辑更新
+	void Update()
+	{
+		UpdateInput();
+		UpdateWorld();
+	}
 
-    void UpdateInput()
-    {
-        var h = Input.GetAxis("Horizontal");
-        var v = Input.GetAxis("Vertical");
+	void UpdateInput()
+	{
+		var h = Input.GetAxis("Horizontal");
+		var v = Input.GetAxis("Vertical");
 
-        var x = Input.GetAxis("Mouse X");
-        var y = Input.GetAxis("Mouse Y");
+		var x = Input.GetAxis("Mouse X");
+		var y = Input.GetAxis("Mouse Y");
 
-        transform.rotation *= Quaternion.Euler(0f, x, 0f);
-        transform.rotation *= Quaternion.Euler(-y, 0f, 0f);
+		transform.rotation *= Quaternion.Euler(0f, x, 0f);
+		transform.rotation *= Quaternion.Euler(-y, 0f, 0f);
 
-        if (Input.GetButton("Jump"))
-        {
-            cc.Move((transform.right * h + transform.forward * v + transform.up) * speed * Time.deltaTime);
-        }
-        else
-        {
-            cc.SimpleMove(transform.right * h + transform.forward * v * speed);
-        }
-    }
+		if (Input.GetButton("Jump"))
+		{
+			cc.Move((transform.right * h + transform.forward * v + transform.up) * speed * Time.deltaTime);
+		}
+		else
+		{
+			cc.SimpleMove(transform.right * h + transform.forward * v * speed);
+		}
+	}
 
-    void UpdateWorld()
-    {
-        for (float x = transform.position.x - viewRange; x < transform.position.x + viewRange; x += Chunk.width)
-        {
-            for (float z = transform.position.z - viewRange; z < transform.position.z + viewRange; z += Chunk.width)
-            {
-                Vector3 pos = new Vector3(x, 0, z);
-                pos.x = Mathf.Floor(pos.x / (float)Chunk.width) * Chunk.width;
-                pos.z = Mathf.Floor(pos.z / (float)Chunk.width) * Chunk.width;
+	void UpdateWorld()
+	{
+		for (float x = transform.position.x - viewRange; x < transform.position.x + viewRange; x += Chunk.width)
+		{
+			for (float z = transform.position.z - viewRange; z < transform.position.z + viewRange; z += Chunk.width)
+			{
+				Vector3 pos = new Vector3(x, 0, z);
+				pos.x = Mathf.Floor(pos.x / (float)Chunk.width) * Chunk.width;
+				pos.z = Mathf.Floor(pos.z / (float)Chunk.width) * Chunk.width;
 
-                Chunk chunk = Chunk.GetChunk(pos);
-                if (chunk != null) continue;
+				Chunk chunk = Chunk.GetChunk(pos);
+				if (chunk != null)
+					continue;
 
-                chunk = (Chunk)Instantiate(chunkPrefab, pos, Quaternion.identity);
-            }
-        }
-    }
+				chunk = (Chunk)Instantiate(chunkPrefab, pos, Quaternion.identity);
+			}
+		}
+	}
+	#endregion
 }
